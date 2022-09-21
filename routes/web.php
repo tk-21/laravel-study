d7c0cf4955d7594baaec39bd281f49139cf51efd
tk <83660733+tk-21@users.noreply.github.com>
2022-09-21 23:41:54 +0900

    ビューファイルの使い方

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/hello_world', function () {
+//     return 'hello world';
+// });
+
+Route::get('/hello_world', fn () => view('hello_world'));
+Route::get('/hello', fn () => view('hello', [
+    'name' => '山田',
+    'course' => 'Laravel9'
+]));
