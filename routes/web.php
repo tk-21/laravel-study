9e288be58786aa5b6509794703a157a584f2ba8b
tk <83660733+tk-21@users.noreply.github.com>
2022-09-23 18:06:19 +0900

    コントローラ

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UtilityController;
+use App\Http\Controllers\GameController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +31,14 @@ Route::get('/hello', fn () => view('hello', [
 
 Route::get('/', fn () => view('index'));
 Route::get('/curriculum', fn () => view('curriculum'));
+
+
+// 世界の時間
+Route::get('/world-time', [UtilityController::class, 'worldTime']);
+
+
+// おみくじ
+Route::get('/omikuji', [GameController::class, 'omikuji']);
+
+// モンティ・ホール問題
+Route::get('/monty-hall', [GameController::class, 'montyHall']);
